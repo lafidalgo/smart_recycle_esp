fd84403735997202f1c55ea4553d09c98331e925
Luiz Augusto Fidalgo <luiz.fidalgo@hotmail.com>
2022-04-05 08:31:39 -0300

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup()
+{
+  lcd.init();
+  lcd.backlight();
+
+  lcd.print("ESP32 - Projeto");
+  lcd.setCursor(0, 1);
+  lcd.print("com display!!");
+}
+
+void loop() {}
